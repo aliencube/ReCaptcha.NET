000b6b4979f42bb9a4b2d6d8a416b0039cb010a3
Justin Yoo <justin.yoo@aliencube.com>
2015-04-16 13:14:37 +1000

    Add ReCaptcha.Wrapper.WebApp project

---
+++ SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aliencube.ReCaptcha.Wrapper.WebApp.MvcApplication" Language="C#" %>
---
+++ SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.ReCaptcha.Wrapper.WebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f4b6dfc-6256-4e1d-899f-ac761c77b545")]

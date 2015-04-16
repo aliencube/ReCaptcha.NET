f32392141060212f840fa453adb04126da02ee38
Justin Yoo <justin.yoo@aliencube.com>
2015-04-16 22:36:56 +1000

    Adjust namespace

--- SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/App_Start/RouteConfig.cs
+++ SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/App_Start/RouteConfig.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ReCaptcha.Wrapper.WebApp
+namespace Aliencube.ReCaptcha.Wrapper.WebApp
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
--- SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/Global.asax.cs
+++ SourceCodes/02_Apps/ReCaptcha.Wrapper.WebApp/Global.asax.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ReCaptcha.Wrapper.WebApp
+namespace Aliencube.ReCaptcha.Wrapper.WebApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
